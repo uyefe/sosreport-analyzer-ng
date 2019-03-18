5fb774325119f4025674818f58d20f6aef39bf7a
fujiwara <shintaro.fujiwara@gmail.com>
2019-03-19 06:56:13 +0900

    New version 	modified:   ChangeLog 	modified:   NEWS 	modified:   VERSION 	modified:   common/global.h 	modified:   configure.ac 	modified:   libsar-analyzer/Makefile.am 	modified:   libsosreport-analyzer/Makefile.am

--- common/global.h
+++ common/global.h
@@ -25,7 +25,7 @@
 
 #define PROGRAM_VERSION 1
 #define PROGRAM_RELEASE 0
-#define PROGRAM_SUB_RELEASE 82
+#define PROGRAM_SUB_RELEASE 83
 
 /* the maximum line length of file name */
 #define MAX_FULL_PATH_LENGTH  512
